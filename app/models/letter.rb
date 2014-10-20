c7f44c36e56949120f4df04db7165b7412a9946e
Drew Martin <drew.e.martin@gmail.com>
2014-10-20 16:15:31 -0400

    letter tests completed

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,2 +1,38 @@
 class Letter < ActiveRecord::Base
+
+  before_save :format_number
+
+  validates :name, :message, :email, :number, presence: true
+
+  validate :email_must_have_symbols
+  validate :number_must_be_valid
+
+  private
+
+  def format_number
+    formatted_number = self.number 
+
+  end
+
+  def email_must_have_symbols
+    unless email.nil?
+
+      if email.split('').include?('@') == false || email.split('').include?('.') == false
+        errors.add(:email, 'must include @ symbol and period')
+      end 
+
+    end  
+  end
+
+  def number_must_be_valid
+    unless number.nil?
+
+      test_val = number.gsub(/[^0-9A-Za-z]/, '')
+      # test_val.delete!' '
+
+      if test_val.length < 10 || test_val.length > 11 || (test_val[0] == '1' && test_val.length == 10)
+       errors.add(:number, 'must include existing non-extension number')
+      end
+    end
+  end
 end
--- spec/models/letter_spec.rb
+++ spec/models/letter_spec.rb
@@ -1,4 +1,55 @@
 require 'rails_helper'
 
 describe Letter do
+  context 'before creations' do
+
+    it 'should have a valid factory' do
+      letter = FactoryGirl.build(:letter)
+      expect(letter).to be_valid
+    end
+
+    it 'should not accept an empty name' do
+      expect(FactoryGirl.build(:letter, name:nil)).to_not be_valid
+    end
+
+    it 'should not have an empty message' do
+      expect(FactoryGirl.build(:letter, message:nil)).to_not be_valid
+    end
+
+    it 'should not have an empty email' do
+      expect(FactoryGirl.build(:letter, email:nil)).to_not be_valid
+    end
+
+    it 'should not have an email w/out @ symbol' do
+      expect(FactoryGirl.build(:letter, email:'bob.com')).to_not be_valid
+    end
+
+    it 'should not have an email w/out @ symbol' do
+      expect(FactoryGirl.build(:letter, email:'bob@com')).to_not be_valid
+    end
+
+    it 'should not have an empty number' do
+      expect(FactoryGirl.build(:letter, number:nil)).to_not be_valid
+    end
+
+    it 'should not have a number w/ too many digits' do
+      expect(FactoryGirl.build(:letter, number:'1-610-610-6100-0')).to_not be_valid
+    end
+
+    it 'should not have a number w/ too few digits' do
+      expect(FactoryGirl.build(:letter, number:'1-610-610-61')).to_not be_valid
+    end
+
+    it 'should not have a number w/ too few digits' do
+      expect(FactoryGirl.build(:letter, number:'1-610-610-61   ')).to_not be_valid
+    end
+
+  end
+
 end
+
+# name Faker::Name.name
+# email Faker::Internet.email
+# number Faker::PhoneNumber.phone_number
+# message "MyText"
+# appointment "2014-10-20 11:55:33"
