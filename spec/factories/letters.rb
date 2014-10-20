c81280e38d111c866dc5697b3b60e19dccee0c1d
Drew Martin <drew.e.martin@gmail.com>
2014-10-20 13:54:50 -0400

    set up letters factory

--- spec/factories/letters.rb
+++ spec/factories/letters.rb
@@ -1,10 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :letter do
-    name "MyString"
-    email "MyString"
-    number "MyString"
+    name Faker::Name.name
+    email Faker::Internet.email
+    number Faker::PhoneNumber.phone_number
     message "MyText"
     appointment "2014-10-20 11:55:33"
   end
