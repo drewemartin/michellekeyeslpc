2e9218fb9465e03d34252755963250d3cf55dcbf
Drew Martin <drew.e.martin@gmail.com>
2014-10-20 13:32:03 -0400

    removed unused methods from letters controller

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,25 +1,11 @@
 class LettersController < ApplicationController
   before_action :set_letter, only: [:show, :edit, :update, :destroy]
 
-  # GET /letters
-  # GET /letters.json
-  def index
-    @letters = Letter.all
-  end
-
-  # GET /letters/1
-  # GET /letters/1.json
-  def show
-  end
-
   # GET /letters/new
   def new
     @letter = Letter.new
   end
 
-  # GET /letters/1/edit
-  def edit
-  end
 
   # POST /letters
   # POST /letters.json
@@ -37,29 +23,8 @@ class LettersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /letters/1
-  # PATCH/PUT /letters/1.json
-  def update
-    respond_to do |format|
-      if @letter.update(letter_params)
-        format.html { redirect_to @letter, notice: 'Letter was successfully updated.' }
-        format.json { render :show, status: :ok, location: @letter }
-      else
-        format.html { render :edit }
-        format.json { render json: @letter.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /letters/1
-  # DELETE /letters/1.json
-  def destroy
-    @letter.destroy
-    respond_to do |format|
-      format.html { redirect_to letters_url, notice: 'Letter was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
